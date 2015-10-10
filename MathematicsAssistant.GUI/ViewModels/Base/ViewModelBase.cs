bd1fe1f316af993c214e36f5abff3bce84f6764b
Martin <martin.guelland@googlemail.com>
2015-10-10 10:44:24 +0200

    new Commit

---
+++ MathematicsAssistant.GUI/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsAssistant.GUI.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void onPropertyChanged(String property)
+        {
+            if (PropertyChanged == null) return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(property));
+        }       
+    }
+}
